a488c4ee5bb67b42b20d39b379989eb8de5fec58
donnelworks <donnel.works@gmail.com>
2022-07-06 16:58:30 +0700

    javascript progress

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-() => {};
+// Init
+const jdl = document.querySelector("#judul");
+const pnl = document.querySelector("#penulis");
+const thn = document.querySelector("#tahun");
+const sts = document.querySelector("#status");
